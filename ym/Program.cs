08513c89b3bd69d7e9954b399b588d6ccd60811b
Joshualahyt <65727098+joshualahyt@users.noreply.github.com>
2022-02-03 22:18:05 +0700

    files

---
+++ ym/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ym
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+
+            Console.Title="iya";
+
+            Console.WriteLine("tidur press any key to continue");
+
+            Console.ReadKey();
+
+            Console.WriteLine("suatu hari yang");
+
+            Console.ReadLine();
+
+            Console.WriteLine("bambang ,tidur, dengan");
+
+            Console.ReadLine();
+
+            Console.WriteLine("menunggu jam 5.50 supaya");
+
+            Console.ReadLine();
+
+            Console.WriteLine("dia pun");
+
+            Console.ReadLine();
+        }
+    }
+}
